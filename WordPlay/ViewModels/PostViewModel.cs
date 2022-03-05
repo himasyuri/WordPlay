729c714abe7f6d1526b330eb8890c99d08aec57d
himasyuri <94618792+himasyuri@users.noreply.github.com>
2022-03-05 17:13:29 +0300

    PostBackend

---
+++ WordPlay/ViewModels/PostViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WordPlay.ViewModels
+{
+    public class PostViewModel
+    {
+        [Required]
+        public string Text { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string UserName { get; set; }
+    }
+}
